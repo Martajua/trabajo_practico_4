545587adfbd47293bc0d6dcc11fce37fd722f62c
martajua <martajua38@gmail.com>
2024-05-28 12:53:27 -0300

    Se creo la clase Materia

---
+++ tp4_JuarezMariaMartadelMilagro/src/main/java/ar/edu/unju/fi/model/Materia.java
@@ -0,0 +1,90 @@
+package ar.edu.unju.fi.model;
+
+public class Materia {
+
+	private String codigoMateria;
+	private String nombreMateria;
+	private int cursoMateria;
+	private int cantidadHorasMateria;
+	private Modalidad modalidadMateria;
+	private Docente docenteMateria;
+	private Carrera carreraMateria;
+	
+	public enum Modalidad{
+		VIRTUAL, PRECENCIAL;
+	}
+
+	public Materia() {
+		super();
+	}
+
+	public Materia(String codigoMateria, String nombreMateria, int cursoMateria, int cantidadHorasMateria,
+			Modalidad modalidadMateria, Docente docenteMateria, Carrera carreraMateria) {
+		super();
+		this.codigoMateria = codigoMateria;
+		this.nombreMateria = nombreMateria;
+		this.cursoMateria = cursoMateria;
+		this.cantidadHorasMateria = cantidadHorasMateria;
+		this.modalidadMateria = modalidadMateria;
+		this.docenteMateria = docenteMateria;
+		this.carreraMateria = carreraMateria;
+	}
+
+	public String getCodigoMateria() {
+		return codigoMateria;
+	}
+
+	public void setCodigoMateria(String codigoMateria) {
+		this.codigoMateria = codigoMateria;
+	}
+
+	public String getNombreMateria() {
+		return nombreMateria;
+	}
+
+	public void setNombreMateria(String nombreMateria) {
+		this.nombreMateria = nombreMateria;
+	}
+
+	public int getCursoMateria() {
+		return cursoMateria;
+	}
+
+	public void setCursoMateria(int cursoMateria) {
+		this.cursoMateria = cursoMateria;
+	}
+
+	public int getCantidadHorasMateria() {
+		return cantidadHorasMateria;
+	}
+
+	public void setCantidadHorasMateria(int cantidadHorasMateria) {
+		this.cantidadHorasMateria = cantidadHorasMateria;
+	}
+
+	public Modalidad getModalidadMateria() {
+		return modalidadMateria;
+	}
+
+	public void setModalidadMateria(Modalidad modalidadMateria) {
+		this.modalidadMateria = modalidadMateria;
+	}
+
+	public Docente getDocenteMateria() {
+		return docenteMateria;
+	}
+
+	public void setDocenteMateria(Docente docenteMateria) {
+		this.docenteMateria = docenteMateria;
+	}
+
+	public Carrera getCarreraMateria() {
+		return carreraMateria;
+	}
+
+	public void setCarreraMateria(Carrera carreraMateria) {
+		this.carreraMateria = carreraMateria;
+	}
+
+}
+
